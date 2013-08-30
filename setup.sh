5de1c89776e5644956cd79059fc11b25b04c0342
Sunil Prakash <prakashsunil@gmail.com>
2013-08-30 15:49:44 +0000

    added setup file

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# for headless setup. 
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git
+sudo apt-get install -y curl
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+
+sudo apt-get -y purge openjdk*
+
+sudo add-apt-repository ppa:webupd8team/java
+
+sudo apt-get -y update && sudo apt-get -y install oracle-java7-installer
+
+sudo apt-get -y install maven
+
+# Load nvm and install latest production node
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get -qq update
+sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+
+
